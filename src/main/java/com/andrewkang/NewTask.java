1a27da9c48a6e00d1c93e0abd3c65340c923179c
Andrew Kang <andrewkang1106@gmail.com>
2021-12-30 14:28:47 -0500

    updated worker class and newtask class

--- src/main/java/com/andrewkang/NewTask.java
+++ src/main/java/com/andrewkang/NewTask.java
@@ -3,6 +3,7 @@ package com.andrewkang;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -17,11 +18,12 @@ public class NewTask {
         factory.setHost("localhost");
         try(Connection connection = factory.newConnection()){
             Channel channel = connection.createChannel();
-            channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+            boolean durable = true;
+            channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
             //String message = "test string" + LocalDateTime.now();
             String message = String.join(" ", args);
 
-            channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes("UTF-8"));
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
             System.out.println("msg: '"+ message + "' has been sent" + LocalDateTime.now());
         }
     }
--- src/main/java/com/andrewkang/Worker.java
+++ src/main/java/com/andrewkang/Worker.java
@@ -14,14 +14,10 @@ public class Worker {
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
-        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        boolean durable = true;
+        channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL + C");
-
-        channel.basicConsume("hello-world", true, (consumerTag, delivery) -> {
-            String m = new String(delivery.getBody(), "UTF-8");
-            System.out.println("I just received a message = " + m);
-        }, consumerTag -> {}); //consumerTag is a tag you get, an ID, from the server when you connect to the server and open up a channel
+        channel.basicQos(1); //only accepts 1 unacknowledged msg at a time
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
            String message = new String(delivery.getBody(), "UTF-8");
@@ -30,15 +26,13 @@ public class Worker {
                doWork(message);
            } finally {
                System.out.println(" [x] Done");
+               //missing basickAck would eat more memory bc messages will be redelieverd when the client quits but won't be released
+               channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
            }
         };
-        boolean autoAck = true;
+        boolean autoAck = false;
         channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
 
-
-        //we're about to tell the server to deliver us the messages from the queue. Since it will push us messages asynchronously,
-        //we provide a callback in the form of an object that will buffer the msg until we're ready to use them
-        //This is what the DeliverCallback subclass does.
     }
     private static void doWork(String task){
         for (char ch: task.toCharArray()){
